820e5f6ff47f384c4a441af4f4d9dd2c6f58ada4
norogoth <52639031+norogoth@users.noreply.github.com>
2019-11-13 20:19:02 -0800

    Add files via upload

--- cat_game/cat_game.py
+++ cat_game/cat_game.py
@@ -9,6 +9,7 @@ fps_clock=pygame.time.Clock()
 
 DISPLAYSURF=pygame.display.set_mode((800,500))
 pygame.display.set_caption('This is fun game ha ha.')
+score=1
 
 #colors
 white=(255,255,255)
@@ -21,8 +22,10 @@ dog_rect=dog_img.get_rect()
 cat_img=pygame.image.load('cat.png')
 food_img=pygame.image.load('food.png')
 food_img=pygame.transform.scale(food_img, (50,30))
+#load sounds
 meow1=pygame.mixer.Sound('meow1.wav')
 meow2=pygame.mixer.Sound('meow2.wav')
+yip=pygame.mixer.Sound('yip.wav')
 
 def meow():
     rand_meow=randint(1,2)
@@ -62,78 +65,99 @@ pressed_up=False
 
 #Lose message
 fontObj = pygame.font.Font('freesansbold.ttf', 32)
-you_lose_text=fontObj.render('You Lose! :(',True,black,teal)
+you_lose_text=fontObj.render('You Lose! :( Your score was {}'.format(score),True,black,teal)
 lose_rect=you_lose_text.get_rect()
 lose_rect.center=(400,200)
 lose=0
-while lose==0:
-    DISPLAYSURF.fill(white)
+#score message
+score_text=fontObj.render(str(score),True,black,white)
+score_rect=score_text.get_rect()
+score_rect.topleft=(0,0)
+bg=pygame.image.load("background.png")
 
-    #pygame.draw.rect(DISPLAYSURF, teal, cat_rect, 1)
-#food bowl hitbox
-    cat_rect = cat_img.get_rect()
-    cat_rect.topleft = ((catx, caty))
-    food_rect=food_img.get_rect()
-    food_rect.topleft=((foodx,foody))
-    #pygame.draw.rect(DISPLAYSURF, teal, food_rect,1)
+running=True
+while running:
+    if lose==0:
+        DISPLAYSURF.fill(white)
+        # pygame.draw.rect(DISPLAYSURF, teal, cat_rect, 1)
+        # food bowl hitbox
+        cat_rect = cat_img.get_rect()
+        cat_rect.topleft = ((catx, caty))
+        dog_rect.topleft = ((dogx, dogy))
+        food_rect = food_img.get_rect()
+        food_rect.topleft = ((foodx, foody))
+        # pygame.draw.rect(DISPLAYSURF, teal, food_rect,1)
 
-#collision detection
-    #dog chase
-    if dogx<=catx:
-        dogx+=2
-    if dogx>=catx:
-        dogx-=2
-    if dogy<=caty:
-        dogy+=2
-    if dogy >= caty:
-        dogy-=2
-    if cat_rect.colliderect(dog_rect):
-        DISPLAYSURF.blit(you_lose_text,lose_rect)
-        lose=1
-    cat_rect.colliderect(food_rect)
-    DISPLAYSURF.blit(cat_img,(catx,caty))
-    DISPLAYSURF.blit(food_img,(foodx,foody))
-    DISPLAYSURF.blit(dog_img,(dogx,dogy))
-    DISPLAYSURF.blit(dog_img,(dogx,dogy))
-
+        # collision detection
+        # dog chase
+        if dogx <= catx:
+            dogx += 3
+        if dogx >= catx:
+            dogx -= 3
+        if dogy <= caty:
+            dogy += 3
+        if dogy >= caty:
+            dogy -= 3
+        if cat_rect.colliderect(dog_rect):
+            yip.play()
+            DISPLAYSURF.blit(you_lose_text, lose_rect)
+            lose = 1
+        cat_rect.colliderect(food_rect)
+        DISPLAYSURF.blit(bg,(0, 0))
+        DISPLAYSURF.blit(cat_img, (catx, caty))
+        DISPLAYSURF.blit(food_img, (foodx, foody))
+        DISPLAYSURF.blit(dog_img, (dogx, dogy))
+        DISPLAYSURF.blit(dog_img, (dogx, dogy))
+        score_text = fontObj.render(str(score), True, black, white)
+        DISPLAYSURF.blit(score_text,score_rect)
+        if lose==1:
+            you_lose_text = fontObj.render('You Lose! :( Your score was {}'.format(score), True, black, teal)
+            DISPLAYSURF.blit(you_lose_text, lose_rect)
+    else: #player has lost game
+        pass
     for event in pygame.event.get():
-        speed=6
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                catx += speed
-                pressed_left=True
-            if event.key == pygame.K_RIGHT:
-                catx -= speed
-                pressed_right=True
-            if event.key==pygame.K_UP:
-                caty-=speed
-                pressed_up=True
-            if event.key==pygame.K_DOWN:
-                caty+=speed
-                pressed_down=True
-        if event.type==pygame.KEYUP:
-            if event.key==pygame.K_LEFT:
-                pressed_left=False
-            if event.key==pygame.K_RIGHT:
-                pressed_right=False
-            if event.key==pygame.K_UP:
-                pressed_up=False
-            if event.key==pygame.K_DOWN:
-                pressed_down=False
-        if event.type==QUIT:
-            pygame.quit()
-            sys.exit()
+        if lose==0:
+            g_rect = dog_img.get_rect()
+            speed=8
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    catx -= speed
+                    pressed_left=True
+                if event.key == pygame.K_RIGHT:
+                    catx += speed
+                    pressed_right=True
+                if event.key==pygame.K_UP:
+                    caty-=speed
+                    pressed_up=True
+                if event.key==pygame.K_DOWN:
+                    caty+=speed
+                    pressed_down=True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    pressed_left = False
+                if event.key == pygame.K_RIGHT:
+                    pressed_right = False
+                if event.key == pygame.K_UP:
+                    pressed_up = False
+                if event.key == pygame.K_DOWN:
+                    pressed_down = False
     if pressed_left:
-        catx-=5
+        catx-=speed
     if pressed_right:
-        catx +=5
+        catx +=speed
     if pressed_up:
-        caty-=5
+        caty-=speed
     if pressed_down:
-        caty+=5
+        caty+=speed
+
     if cat_rect.colliderect(food_rect)==True:
         foodx = randint(0, 700)
         foody = randint(0, 400)
+        score+=1
         meow()
+    if event.type==pygame.QUIT:
+        running=False
+    else: #player has lost game
+        pass
     pygame.display.update()
     fps_clock.tick(fps)
