4776a191ca77e5f1525df499bd1885854e89aab7
norogoth <52639031+norogoth@users.noreply.github.com>
2020-01-13 13:52:30 -0800

    Update cat_game.py

--- cat_game/cat_game.py
+++ cat_game/cat_game.py
@@ -7,82 +7,82 @@ image_path=directory_path+'image.png'
 
 pygame.init()
 
-fps=30
+fps = 30
 fps_clock=pygame.time.Clock()
 
-DISPLAYSURF=pygame.display.set_mode((800,500))
+DISPLAYSURF = pygame.display.set_mode((800,500))
 pygame.display.set_caption('This is fun game ha ha.')
-score=1
+score = 1
 
 #colors
-white=(255,255,255)
-black=(0,0,0)
-teal=(0,150,80)
+white = (255,255,255)
+black = (0,0,0)
+teal = (0,150,80)
 
 #load images
-dog_img=pygame.image.load('doggo.png')
-dog_rect=dog_img.get_rect()
-cat_img=pygame.image.load('cat.png')
-food_img=pygame.image.load('food.png')
-food_img=pygame.transform.scale(food_img, (50,30))
+dog_img = pygame.image.load('doggo.png')
+dog_rect = dog_img.get_rect()
+cat_img = pygame.image.load('cat.png')
+food_img = pygame.image.load('food.png')
+food_img = pygame.transform.scale(food_img, (50,30))
 #load sounds
-music=pygame.mixer.music.load('cat_game_music.wav')
-meow1=pygame.mixer.Sound('meow1.wav')
-meow2=pygame.mixer.Sound('meow2.wav')
-yip=pygame.mixer.Sound('yip.wav')
+music = pygame.mixer.music.load('cat_game_music.wav')
+meow1 = pygame.mixer.Sound('meow1.wav')
+meow2 = pygame.mixer.Sound('meow2.wav')
+yip = pygame.mixer.Sound('yip.wav')
 
 def meow():
-    rand_meow=randint(1,2)
-    if rand_meow==1:
+    rand_meow = randint(1,2)
+    if rand_meow == 1:
         meow1.play()
-    if rand_meow==2:
+    if rand_meow == 2:
         meow2.play()
 
 #coordinates of cat, the later, dog
-catx=randint(0,700)
-caty=randint(0,400)
+catx = randint(0,700)
+caty = randint(0,400)
 
 #cat hitbox
 cat_rect = cat_img.get_rect()
-cat_rect.topleft=((catx,caty))
+cat_rect.topleft = ((catx,caty))
 def dog():
     global dogx
     global dogy
-    dogx=randint(0,700)
-    dogy=randint(0,400)
+    dogx = randint(0,700)
+    dogy = randint(0,400)
     while True:
         if cat_rect.colliderect(dog_rect):
             dogx = randint(0,700)
-            dogy=randint(0,400)
+            dogy = randint(0,400)
         else:
             break
 dog()
-foodx=randint(0,700)
-foody=randint(0,400)
+foodx = randint(0,700)
+foody = randint(0,400)
 
-direction='right'
+direction = 'right'
 
-pressed_left=False
-pressed_right=False
-pressed_down=False
-pressed_up=False
+pressed_left = False
+pressed_right = False
+pressed_down = False
+pressed_up = False
 
 #Lose message
 fontObj = pygame.font.Font('freesansbold.ttf', 32)
-you_lose_text=fontObj.render('You Lose! :( Your score was {}'.format(score),True,black,teal)
-lose_rect=you_lose_text.get_rect()
-lose_rect.center=(400,200)
-lose=0
+you_lose_text = fontObj.render('You Lose! :( Your score was {}'.format(score),True,black,teal)
+lose_rect = you_lose_text.get_rect()
+lose_rect.center = (400,200)
+lose = 0
 #score message
-score_text=fontObj.render(str(score),True,black,white)
-score_rect=score_text.get_rect()
-score_rect.topleft=(0,0)
-bg=pygame.image.load("background.png")
+score_text = fontObj.render(str(score),True,black,white)
+score_rect = score_text.get_rect()
+score_rect.topleft = (0,0)
+bg = pygame.image.load("background.png")
 
 running=True
 pygame.mixer.music.play(-1)
 while running:
-    if lose==0:
+    if lose == 0:
         DISPLAYSURF.fill(white)
         # pygame.draw.rect(DISPLAYSURF, teal, cat_rect, 1)
         # food bowl hitbox
@@ -123,22 +123,22 @@ while running:
     else: #player has lost game
         pass
     for event in pygame.event.get():
-        if lose==0:
+        if lose == 0:
             g_rect = dog_img.get_rect()
-            speed=8
+            speed = 8
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     catx -= speed
-                    pressed_left=True
+                    pressed_left = True
                 if event.key == pygame.K_RIGHT:
                     catx += speed
-                    pressed_right=True
+                    pressed_right = True
                 if event.key==pygame.K_UP:
                     caty-=speed
-                    pressed_up=True
+                    pressed_up = True
                 if event.key==pygame.K_DOWN:
                     caty+=speed
-                    pressed_down=True
+                    pressed_down = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     pressed_left = False
@@ -149,21 +149,21 @@ while running:
                 if event.key == pygame.K_DOWN:
                     pressed_down = False
     if pressed_left:
-        catx-=speed
+        catx -= speed
     if pressed_right:
-        catx +=speed
+        catx += speed
     if pressed_up:
-        caty-=speed
+        caty -= speed
     if pressed_down:
-        caty+=speed
+        caty += speed
 
     if cat_rect.colliderect(food_rect)==True:
         foodx = randint(0, 700)
         foody = randint(0, 400)
-        score+=1
+        score += 1
         meow()
-    if event.type==pygame.QUIT:
-        running=False
+    if event.type == pygame.QUIT:
+        running = False
     else: #player has lost game
         pass
     pygame.display.update()
